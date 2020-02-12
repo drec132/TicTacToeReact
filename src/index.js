1e566efcf0795d18d0f722c3f10d97badd4fca7d
Jonerec Cereno <cjonerec@gmail.com>
2020-02-13 00:02:43 +0800

    Modified to add the index.css styles and modified the index.js for 'Completing the Game' in the tutorial

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button 
-            className="square" 
-            onClick={() => this.props.onClick}>
+            <button
+                className="square"
+                onClick={() => this.props.onClick()}>
                 {this.props.value}
             </button>
         );
@@ -20,26 +20,38 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares})
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({ squares: squares, xIsNext: !this.state.xIsNext })
     }
 
     renderSquare(i) {
         return (
-            <Square 
+            <Square
                 value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)} 
+                onClick={() => this.handleClick(i)}
             />
         );
     }
 
     render() {
-        const status = 'Next player: X';
+        let status;
+
+        const winner = calculateWinner(this.state.squares);
+
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -56,6 +68,28 @@ class Board extends React.Component {
             </div>
         );
     }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] 
+            && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 class Game extends React.Component {
